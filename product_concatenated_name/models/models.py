a4137cdabc77bf447083deb749b365d0edaed472
hroque-octupus <hroque@octupus.es>
2020-03-09 13:58:21 -0500

    Initial commit.

---
+++ product_concatenated_name/models/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.onchange('brand')
+    def change_product_name(self):
+        pass
