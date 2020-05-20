5d04cd8745a79c507984f18466c2f2577e6b718a
nikeshpydev <nikeshpydev@gmail.com>
2020-05-20 14:05:01 +0530

    Added quantity option in the product list

--- website_sale_custom/__manifest__.py
+++ website_sale_custom/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'E-Commerce',
+    'name': 'E-Commerce Custom',
     'author': 'MaxStackLabs',
     'description': '''Add authentication for shop and custom fields''',
     'category': 'Website',
     'depends': ['website_sale'],
     'data': [
+        'views/website_template_inherited.xml'
     ],
     'installable': True,
     'auto_install': False,
