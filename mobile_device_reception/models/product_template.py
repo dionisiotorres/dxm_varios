ae685aa4bd9b0ce0a0ff433426acaa98c4d54487
sergio <sergio@webcuba.es>
2020-08-03 15:39:00 +0200

    [fix] sku

--- mobile_device_reception/models/product_template.py
+++ mobile_device_reception/models/product_template.py
@@ -20,11 +20,18 @@ class ProductTemplate(models.Model):
                     model_sku_part = record.x_studio_model_code or record.x_studio_modelo
                     model_sku_part = model_sku_part[0:3].upper()
                     if record.x_studio_part_number:
-                        part_number_sku_part = record.x_studio_part_number[0:4].upper()
+                        part_number_sku_part = record.x_studio_part_number.upper()
                         if record.x_studio_capacidad_de_almacenamiento:
                             capacity_sku_part = record.x_studio_capacidad_de_almacenamiento.x_name.replace(' ', '')[0:3]
                             record[
                                 'product_sku'] = brand_sku_part + model_sku_part + part_number_sku_part + capacity_sku_part
                             sku_completed = True
+                    else:
+                        if record.x_studio_capacidad_de_almacenamiento:
+                            capacity_sku_part = record.x_studio_capacidad_de_almacenamiento.x_name.replace(' ', '')[0:3]
+                            record[
+                                'product_sku'] = brand_sku_part + model_sku_part + capacity_sku_part
+                            sku_completed = True
+
             if not sku_completed:
                 record['product_sku'] = False
